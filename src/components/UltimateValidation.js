60498a74f7494c060f7beba71ab86e8759b7c818
Lê Minh Quân <marucube35@gmail.com>
2022-07-25 17:32:58 +0700

    style (validation): finish CSS

--- src/components/UltimateValidation.js
+++ src/components/UltimateValidation.js
@@ -124,196 +124,210 @@ export default class UltimateValidation extends Component {
         const errors = this.validate()
         return (
             <div className="validation-wrapper">
-                <h3>Add Student</h3>
-                <form onSubmit={this.handleSubmit} noValidate>
-                    <div className="form-group">
-                        <label htmlFor="firstName">First Name </label>
-                        <input
-                            type="text"
-                            name="firstName"
-                            value={this.state.firstName}
-                            onChange={this.handleChange}
-                            onBlur={this.handleBlur}
-                            placeholder="First Name"
-                        />
-                        <br />
-                        <small>{errors.firstName}</small>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="lastName">Last Name </label>
-                        <input
-                            type="text"
-                            name="lastName"
-                            value={this.state.lastName}
-                            onChange={this.handleChange}
-                            onBlur={this.handleBlur}
-                            placeholder="Last Name"
-                        />
-                         <br />
-                        <small>{errors.lastName}</small>
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="email">Email </label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                            placeholder="Email"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="tel">Telephone </label>
-                        <input
-                            type="tel"
-                            name="tel"
-                            value={this.state.tel}
-                            onChange={this.handleChange}
-                            placeholder="Tel"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="dateOfBirth">Date of birth </label>
-                        <input
-                            type="date"
-                            name="dateOfBirth"
-                            value={this.state.dateOfBirth}
-                            onChange={this.handleChange}
-                            placeholder="Date of Birth"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="favoriteColor">Favorite Color</label>
-                        <input
-                            // type="color"
-                            id="favoriteColor"
-                            name="favoriteColor"
-                            value={this.state.favoriteColor}
-                            onChange={this.handleChange}
-                            placeholder="Favorite Color"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="weight">Weight </label>
-                        <input
-                            type="number"
-                            id="weight"
-                            name="weight"
-                            value={this.state.weight}
-                            onChange={this.handleChange}
-                            placeholder="Weight in Kg"
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label htmlFor="country">Country</label> <br />
-                        <select
-                            name="country"
-                            onChange={this.handleChange}
-                            id="country"
-                        >
-                            {selectOptions}
-                        </select>
-                    </div>
-
-                    <div className="form-group">
-                        <p>Gender</p>
-                        <div>
+                <h3 className="validation-title">Ultimate Form</h3>
+                <form
+                    onSubmit={this.handleSubmit}
+                    noValidate
+                    className="validation-form"
+                >
+                    <div className="form-groups">
+                        <div className="form-group">
+                            <label htmlFor="firstName">First Name </label>
                             <input
-                                type="radio"
-                                id="female"
-                                name="gender"
-                                value="Female"
+                                type="text"
+                                id="firstName"
+                                name="firstName"
+                                value={this.state.firstName}
                                 onChange={this.handleChange}
-                                checked={this.state.gender === 'Female'}
+                                onBlur={this.handleBlur}
+                                placeholder="First Name"
                             />
-                            <label htmlFor="female">Female</label>
+                            <small className="form-error">
+                                {errors.firstName}
+                            </small>
                         </div>
-                        <div>
-                            <input
-                                id="male"
-                                type="radio"
-                                name="gender"
-                                value="Male"
-                                onChange={this.handleChange}
-                                checked={this.state.gender === 'Male'}
-                            />
-                            <label htmlFor="male">Male</label>
-                        </div>
-                        <div>
-                            <input
-                                id="other"
-                                type="radio"
-                                name="gender"
-                                value="Other"
-                                onChange={this.handleChange}
-                                checked={this.state.gender === 'Other'}
-                            />
-                            <label htmlFor="other">Other</label>
-                        </div>
-                    </div>
 
-                    <div className="form-group">
-                        <p>Select your skills</p>
-                        <div>
+                        <div className="form-group">
+                            <label htmlFor="lastName">Last Name </label>
                             <input
-                                type="checkbox"
-                                id="html"
-                                name="html"
+                                type="text"
+                                name="lastName"
+                                value={this.state.lastName}
+                                onChange={this.handleChange}
+                                onBlur={this.handleBlur}
+                                placeholder="Last Name"
+                            />
+                            <small className="form-error">
+                                {errors.lastName}
+                            </small>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="email">Email </label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={this.state.email}
+                                onChange={this.handleChange}
+                                onBlur={this.handleBlur}
+                                placeholder="Email"
+                            />
+                            <small className="form-error">{errors.email}</small>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="tel">Telephone </label>
+                            <input
+                                type="tel"
+                                name="tel"
+                                value={this.state.tel}
+                                onChange={this.handleChange}
+                                placeholder="Tel"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="dateOfBirth">Date of birth </label>
+                            <input
+                                type="date"
+                                name="dateOfBirth"
+                                value={this.state.dateOfBirth}
+                                onChange={this.handleChange}
+                                placeholder="Date of Birth"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="favoriteColor">
+                                Favorite Color
+                            </label>
+                            <input
+                                type="color"
+                                id="favoriteColor"
+                                name="favoriteColor"
+                                value={this.state.favoriteColor}
+                                onChange={this.handleChange}
+                                placeholder="Favorite Color"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="weight">Weight </label>
+                            <input
+                                type="number"
+                                id="weight"
+                                name="weight"
+                                value={this.state.weight}
+                                onChange={this.handleChange}
+                                placeholder="Weight in Kg"
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="country">Country</label>
+                            <select
+                                name="country"
+                                onChange={this.handleChange}
+                                id="country"
+                            >
+                                {selectOptions}
+                            </select>
+                        </div>
+
+                        <div className="form-group">
+                            <p>Gender</p>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="female"
+                                    name="gender"
+                                    value="Female"
+                                    onChange={this.handleChange}
+                                    checked={this.state.gender === 'Female'}
+                                />
+                                <label htmlFor="female">Female</label>
+                            </div>
+                            <div>
+                                <input
+                                    id="male"
+                                    type="radio"
+                                    name="gender"
+                                    value="Male"
+                                    onChange={this.handleChange}
+                                    checked={this.state.gender === 'Male'}
+                                />
+                                <label htmlFor="male">Male</label>
+                            </div>
+                            <div>
+                                <input
+                                    id="other"
+                                    type="radio"
+                                    name="gender"
+                                    value="Other"
+                                    onChange={this.handleChange}
+                                    checked={this.state.gender === 'Other'}
+                                />
+                                <label htmlFor="other">Other</label>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <p>Select your skills</p>
+                            <div>
+                                <input
+                                    type="checkbox"
+                                    id="html"
+                                    name="html"
+                                    onChange={this.handleChange}
+                                />
+                                <label htmlFor="html">HTML</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="checkbox"
+                                    id="css"
+                                    name="css"
+                                    onChange={this.handleChange}
+                                />
+                                <label htmlFor="css">CSS</label>
+                            </div>
+                            <div>
+                                <input
+                                    type="checkbox"
+                                    id="javascript"
+                                    name="javascript"
+                                    onChange={this.handleChange}
+                                />
+                                <label htmlFor="javascript">JavaScript</label>
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="bio">Bio</label>
+                            <textarea
+                                id="bio"
+                                name="bio"
+                                value={this.state.bio}
+                                onChange={this.handleChange}
+                                cols="120"
+                                rows="10"
+                                placeholder="Write about yourself ..."
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="file">File</label>
+                            <input
+                                type="file"
+                                name="file"
+                                id="file"
                                 onChange={this.handleChange}
                             />
-                            <label htmlFor="html">HTML</label>
-                        </div>
-                        <div>
-                            <input
-                                type="checkbox"
-                                id="css"
-                                name="css"
-                                onChange={this.handleChange}
-                            />
-                            <label htmlFor="css">CSS</label>
-                        </div>
-                        <div>
-                            <input
-                                type="checkbox"
-                                id="javascript"
-                                name="javascript"
-                                onChange={this.handleChange}
-                            />
-                            <label htmlFor="javascript">JavaScript</label>
                         </div>
                     </div>
-
-                    <div className="form-group">
-                        <label htmlFor="bio">Bio</label> <br />
-                        <textarea
-                            id="bio"
-                            name="bio"
-                            value={this.state.bio}
-                            onChange={this.handleChange}
-                            cols="120"
-                            rows="10"
-                            placeholder="Write about yourself ..."
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <input
-                            type="file"
-                            name="file"
-                            onChange={this.handleChange}
-                        />
-                    </div>
-
-                    <div>
-                        <button>Submit</button>
-                    </div>
+                    <button type="submit" className="form-button">
+                        Submit
+                    </button>
                 </form>
             </div>
         )
